a706ae73888beb155c4277768d84b117ac89e111
Innei <i@innei.in>
2024-09-16 15:05:51 +0800

    init

--- api/index.ts
+++ api/index.ts
@@ -1,34 +1,4 @@
-import "./lib/load-env"
-
-import { createRequire } from "node:module"
-
-import middie from "@fastify/middie"
-import Fastify from "fastify"
-
-import { globalRoute } from "./router/global"
-import { ogRoute } from "./router/og"
-
-const app = Fastify({})
-
-await app.register(middie, {
-  hook: "onRequest",
-})
-
-const require = createRequire(import.meta.url)
-
-if (process.env.NODE_ENV === "development") {
-  const devVite = require("./lib/dev-vite")
-  await devVite.registerDevViteServer(app)
-}
-
-ogRoute(app)
-globalRoute(app)
-
-const isVercel = process.env.VERCEL === "1"
-if (!isVercel) {
-  await app.listen({ port: 2233 })
-  console.info("Server is running on http://localhost:2233")
-}
+import { app } from "../server"
 
 export default async function handler(req: any, res: any) {
   await app.ready()
--- api/lib/load-env.ts
+++
@@ -1,9 +0,0 @@
-import { dirname, resolve } from "node:path"
-import { fileURLToPath } from "node:url"
-
-import { config } from "dotenv"
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-config({
-  path: resolve(__dirname, "../../.env"),
-})
