106765e8f2c57f75caeef7b3a94fa724c4572050
Innei <i@innei.in>
2024-09-16 16:04:49 +0800

    update
    Signed-off-by: Innei <i@innei.in>

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ["api/index.ts"],
+  entry: ["server/api/index.ts"],
   splitting: true,
   sourcemap: true,
   clean: true,
   format: ["esm"],
 
-  outDir: "./out/server",
+  outDir: "./api",
 })
